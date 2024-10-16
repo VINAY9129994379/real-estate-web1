564f0f4c9d020b6552b463225eeec3c06c895419
VINAY9129994379 <vinay2272001@gmail.com>
2024-10-16 23:27:46 +0530

    first commit

---
+++ frontend/src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+function Hero() {
+  return (
+    <div className='hero'>
+        <div className='hero-contain'>
+            <h1 className='hero-heading'>New Properties</h1>
+            <p className='hero-para'>EXCLUSIVELY BT VINIYAS</p>
+            <br/>
+            <button className='hero-btn'>Explore</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ frontend/src/components/NewsLetter.js
@@ -0,0 +1,237 @@
+import React from 'react'
+import './NewsLetter.css'
+import NewsImg1 from '../assets/NewsImg1.png'
+function NewsLetter() {
+  return (
+    <div className='news-letter'>
+        <div className='news-letter-left-contain'>
+    <div className='news-letter-left'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt=''/>
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/>
+<div className='news-letter-left'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt=''/>
+
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/>
+<div className='news-letter-left'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt="Apartment" /> 
+
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/>
+<div className='news-letter-left'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt="Apartment" /> 
+
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/>
+<div className='news-letter-left'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt=''/>
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/>
+<div className='news-letter-left'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt=''/>
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+</div>
+
+
+
+<div className='news-letter-right-contain'>
+<div className='news-letter-right'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt="Apartment" /> 
+
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/>
+<div className='news-letter-right'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt="Apartment" /> 
+
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/>
+<div className='news-letter-right'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt="Apartment" /> 
+
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/>
+<div className='news-letter-right'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt="Apartment" /> 
+
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/><div className='news-letter-right'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt="Apartment" /> 
+
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>
+<br/>
+<br/><div className='news-letter-right'>
+            <p>Nov 1 2035</p>
+            <h1>Choosing Your Next Apartment</h1>
+            <p> BY Mike Helman</p>
+            <img src={NewsImg1} alt="Apartment" /> 
+
+            <br/>
+            <p>This item is connected to a text field in your content collection. Double<br/> click what you want to edit and then select "Change Content" to add<br/> your own content to the collection. Want to view and manage all your<br/> collections? Click the Content Manager icon on the add panel to your<br/> left. In the Content Manager, you can update items,<br/> add new fields, create dynamic pages and more.
+
+</p>
+<br/>
+<br/>
+<p>Your content collection is already set up with fields and content. Add<br/> your own by editing each field, or import CSV files to your content <br/>collection. You can create fields for rich content, images, videos and<br/> more.
+</p>
+<br/>
+<button className='read-btn'>Read More</button>
+</div>        
+        </div>
+    </div>
+  )
+}
+
+export default NewsLetter
